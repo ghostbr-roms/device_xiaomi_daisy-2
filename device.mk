7dbb95bb3533c649a8bed4f17645b46b5e221174
John Dias <joaodias@google.com>
2017-06-21 13:14:52 -0700

    daisy: Pre-opt SystemUI
    SystemUI is getting deoptimized, possibly resulting in jank
    (not yet validated); pre-optimize to avoid deoptimization for now.
    Bug: 62832717
    Bug: 35872915
    Test: boot, check for vdex, run for a bit
    
    Change-Id: Ia754db444eebaebf0ff2389ab66f6f1d486da994

--- device.mk
+++ device.mk
@@ -123,6 +123,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     chargeonlymode
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
