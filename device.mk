91e96a9a8e7a02ca7e64e9a593506b8734790be1
Bruno Martins <bgcngm@gmail.com>
2019-01-10 11:51:54 +0100

    daisy: Convert XiaomiDoze into XiaomiParts
     * This is the preparation for turning the package into
       an unified device-settings app.
    
    Change-Id: I255f1ab8dab0e1037dee766a5ba5ee9f62fdfe6d

--- device.mk
+++ device.mk
@@ -76,9 +76,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.display.config@1.0
 
-# Doze
+# Device-specific settings
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    XiaomiParts
 
 # FM
 PRODUCT_PACKAGES += \
